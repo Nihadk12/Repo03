ba88abc0e6407095e0a955ac0601dca12474c6cf
Nihadk12 <kadic_niho@hotmail.com>
2021-09-15 17:56:19 +0200

    minus

---
+++ ex09/src/index.js
@@ -0,0 +1,16 @@
+function main(numOne, numTwo) {
+    // Only change code below this line
+
+    var differce;
+
+    differce = numOne - numTwo;
+
+    // Only change code above this line
+
+
+    return differce;
+}
+
+console.log(main(1500,695));
+
+module.exports = main;

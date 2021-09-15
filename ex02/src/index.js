88927d5c75bb0d6fb1ced60a27c1ec3215e44010
Nihadk12 <kadic_niho@hotmail.com>
2021-09-15 16:01:48 +0200

    comment is exist

---
+++ ex02/src/index.js
@@ -0,0 +1,18 @@
+function main() {
+    // Only change code below this line
+
+    //This is a sinle line JavaScript comment
+
+    /* document.wrtie("This is a multi-line comment");
+         document.wrtie("This is a multi-line comment");
+          document.wrtie("This is a multi-line comment");
+           document.wrtie("This is a multi-line comment");
+            document.wrtie("This is a multi-line comment");
+    */
+
+    // Only change code above this line
+}
+
+console.log(main());
+
+module.exports = main;

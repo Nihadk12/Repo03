ab0f28f9db10db51429d024988ababc5cea6a3aa
Nihadk12 <kadic_niho@hotmail.com>
2021-09-15 17:45:16 +0200

    sum

---
+++ ex08/src/index.js
@@ -0,0 +1,12 @@
+function main(numOne, numTwo) {
+    // Only change code below this line
+    var sum;
+   sum = numOne + numTwo;
+
+    // Only change code above this line
+
+    return sum;
+}
+console.log(main(15,8));
+
+module.exports = main;

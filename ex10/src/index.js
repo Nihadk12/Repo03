614050055438f01ae2411b94545baf0943044411
Nihadk12 <kadic_niho@hotmail.com>
2021-09-15 18:01:17 +0200

    mnozenje

---
+++ ex10/src/index.js
@@ -0,0 +1,16 @@
+function main(numOne, numTwo) {
+    // Only change code below this line
+
+    var product;
+
+    product = numOne * numTwo;
+
+    // Only change code above this line
+
+
+    return product;
+}
+
+console.log(main(15,6));
+
+module.exports = main;

eadd1cf5b858b1e8b370b527898a1308dc6cb303
Nihadk12 <kadic_niho@hotmail.com>
2021-09-15 17:36:49 +0200

    okej

---
+++ ex07/src/index.js
@@ -0,0 +1,23 @@
+var studlyCapVar = 10;
+var properCamelCase = "A String";
+var titleCaseOver = 9000;
+
+
+
+function main() {
+    //Only change code below this line
+
+    //Variable assignemnts
+
+    //Only change code apove this line
+    return {
+        studlyCapVar,
+        properCamelCase,
+        titleCaseOver
+        } 
+}
+
+console.log(main());
+
+module.exports = main;
+
